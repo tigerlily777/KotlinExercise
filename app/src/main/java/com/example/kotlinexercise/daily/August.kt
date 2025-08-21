2aaedab5a43db75712f6e42fb0600079dcad49ca
Tiger HU <tigerlilyh777@gmail.com>
2025-08-21 23:51:44 +1200

    Use filter and map

--- app/src/main/java/com/example/kotlinexercise/daily/August.kt
+++ app/src/main/java/com/example/kotlinexercise/daily/August.kt
@@ -6,6 +6,7 @@ data class User(
     val age: Int
 )
 
+
 fun main() {
     val original = User(1, "Tiger", 30)
 
@@ -13,7 +14,27 @@ fun main() {
     val updated = original.copy(
         name = "Tigerlily777"
     )
-
+    // 1. use copy to create a new User object with updated name
     println("Original: $original")
     println("Updated: $updated")
+
+    val users = listOf(
+        User(1, "Alice", 16),
+        User(2, "Bob", 22),
+        User(3, "Tiger", 30),
+        User(4, "Lily", 18)
+    )
+
+    // 2. use filter to get a list of users older than 18
+    val adults: List<User> = users.filter {
+        user -> user.age > 18
+    }
+
+    // 3. use map to get a list of names of the users older than 18
+    val names: List<String> = adults.map {
+        user -> user.name
+    }
+
+    println("Adults: $adults")
+    println("Adult names: $names")
 }
